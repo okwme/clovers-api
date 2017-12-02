7884fc58432e86d15aaec8c0ceeebe8c6ced3c08
Billy Rennekamp <billy.rennekamp@gmail.com>
2017-12-02 12:32:37 +0100

    serving?

--- server/app.js
+++ server/app.js
@@ -59,9 +59,31 @@ var {
 console.log('hello world')
 
 
-var io = require('socket.io')(3333);
+// We’re setting up an extremely simple server here.
+const http = require('http');
 
+// These could (should) be set as env vars.
+const port = process.env.PORT || 3333;
+const host = process.env.HOST || 'localhost';
 
+// No matter what hits the server, we send the same thing.
+var server = http.createServer((req, res) => {
+
+  // Tell the browser what’s coming.
+  res.writeHead(200, {
+    'Content-Type': 'text/html; charset=utf-8',
+  });
+
+  // Send a simple message in HTML.
+  res.write('<h1>I’m a Node app!</h1>');
+  res.write('<p>And I’m <em>sooooo</em> secure.</p>');
+  res.end();
+}).listen(port, host);
+
+// This message prints in the console when the app starts.
+console.log(`App running at http://${host}:${port}`);
+
+var io = require('socket.io')(server);
 
 
 
